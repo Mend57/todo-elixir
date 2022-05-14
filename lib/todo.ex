9fab1be2ffc8e931415e27d94282471545c76b0d
Mend57 <cury.ro@gmail.com>
2022-05-14 00:06:33 -0300

    msg

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,15 +1,32 @@
 defmodule Todo do
 
-  def add1 num do
-    num + 1
+  def parse(input, type) do
+    {num, _} = type.parse(input)
+    num
   end
 
-  def mult5 num do
-    num * 5
+  def contains(tasks, target) do
+    Enum.member?(tasks, target)
   end
 
-  def add1thenmult5 do
-    mult5(add1(5))
+  def random_task(tasks) do
+    [task] = Enum.take_random(tasks, 1)
+    task
   end
+
+  def todos_list do
+    ['a', 'b', 'c', 'd', 'e']
+  end
+
+  def create_todos do
+    num_tasks = IO.gets("Enter the number of tasks you want to add to your list: ") |> parse(Integer)
+    task = todos_list() |> random_task()
+
+    if [1,2,3] |> contains(num_tasks) do
+      IO.puts(task)
+    end
+  end
+
+  # mix run -e Todo.
 
 end
