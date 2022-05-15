45504701a32c84b2de65f4b484d8d8e3a92c08c3
Mend57 <cury.ro@gmail.com>
2022-05-15 16:12:04 -0300

    save system

--- lib/todo.ex
+++ lib/todo.ex
@@ -27,10 +27,11 @@ defmodule Todo do
   end
 
   def return_remove_task(list) do
+    save(list)
     option = parse(Integer, IO.gets("\n1- Remove another task\n2- Return to menu\n"))
     case option do
       1 -> remove_task(list)
-      2 -> create_todos()
+      2 -> main()
     end
   end
 
@@ -42,13 +43,8 @@ defmodule Todo do
     option = parse(Integer, IO.gets("\n1- Get another random task\n2- Return to menu\n"))
     case option do
       1 -> random_task(tasks)
-      2 -> create_todos()
+      2 -> main()
     end
-  end
-
-
-  def todos_list do
-    ["banana", "apple", "orange"]
   end
 
 
@@ -56,7 +52,7 @@ defmodule Todo do
     IO.puts("")
     Enum.map(list, fn x -> IO.puts("- " <> x) end)
     IO.gets("\nPress 'enter' to return to menu: ")
-    create_todos()
+    main()
   end
 
   def num_inputs(list) do
@@ -72,28 +68,40 @@ defmodule Todo do
       list = List.insert_at(list, -1, task)
       input_tasks(list, old_length, num_tasks)
     else
+      save(list)
       option = parse(Integer, IO.gets("\n1- Insert another task\n2- Return to menu\n"))
       case option do
         1 -> num_inputs(list)
-        2 -> create_todos()
+        2 -> main()
       end
     end
   end
 
-  def create_todos do
-    option = parse(Integer, IO.gets("\n1- See your list\n2- Add tasks to your list\n3- Remove a task from your list\n4- Choose a random task\n5- \n6- Sair\n"))
+
+  def save(tasks) do
+    binary = :erlang.term_to_binary(tasks)
+    File.write("todos", binary)
+  end
+
+  def todos_list do
+    {_, binary_data} = File.read("todos")
+    :erlang.binary_to_term(binary_data)
+  end
+
+
+  def main do
+    option = parse(Integer, IO.gets("\n1- See your list\n2- Add tasks to your list\n3- Remove a task from your list\n4- Choose a random task\n5- Sair\n"))
 
     case option do
       1 -> todos_list() |> see_list()
       2 -> todos_list() |> num_inputs()
       3 -> todos_list() |> remove_task()
       4 -> todos_list() |> random_task()
-      # 5 ->
-      6 -> exit("")
-
+      5 -> exit("")
+      _ -> {IO.puts("Invalid response."), main()}
     end
   end
 
-  # mix run -e Todo.
+  # mix run -e Todo.main
 
 end
